a7382da7ca161a22c738568593e6012f5095dba4
David Quarel <davidquarel@gmail.com>
2024-05-19 17:21:30 +0000

    disable tuned lens for sweep (only looking at last later anyway)

---
+++ big_run_move.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_fr_en_zh --src_lang fr --dest_lang zh --latent_lang en --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_fr_en_de --src_lang fr --dest_lang de --latent_lang en --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_fr_zh_en --src_lang fr --dest_lang en --latent_lang zh --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_fr_zh_de --src_lang fr --dest_lang de --latent_lang zh --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_fr_de_en --src_lang fr --dest_lang en --latent_lang de --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_fr_de_zh --src_lang fr --dest_lang zh --latent_lang de --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_en_fr_zh --src_lang en --dest_lang zh --latent_lang fr --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_en_fr_de --src_lang en --dest_lang de --latent_lang fr --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_en_zh_fr --src_lang en --dest_lang fr --latent_lang zh --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_en_zh_de --src_lang en --dest_lang de --latent_lang zh --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_en_de_fr --src_lang en --dest_lang fr --latent_lang de --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_en_de_zh --src_lang en --dest_lang zh --latent_lang de --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_zh_fr_en --src_lang zh --dest_lang en --latent_lang fr --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_zh_fr_de --src_lang zh --dest_lang de --latent_lang fr --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_zh_en_fr --src_lang zh --dest_lang fr --latent_lang en --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_zh_en_de --src_lang zh --dest_lang de --latent_lang en --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_zh_de_fr --src_lang zh --dest_lang fr --latent_lang de --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_zh_de_en --src_lang zh --dest_lang en --latent_lang de --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_de_fr_en --src_lang de --dest_lang en --latent_lang fr --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_de_fr_zh --src_lang de --dest_lang zh --latent_lang fr --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_de_en_fr --src_lang de --dest_lang fr --latent_lang en --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_de_en_zh --src_lang de --dest_lang zh --latent_lang en --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_de_zh_fr --src_lang de --dest_lang fr --latent_lang zh --intervention_func 'hook_diff_subspace' --use_tuned_lens False
+python3 llama_experiment_template.py --log_file out/move_lang_sweep/hook_move_de_zh_en --src_lang de --dest_lang en --latent_lang zh --intervention_func 'hook_diff_subspace' --use_tuned_lens False
